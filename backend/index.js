ea31ee17e36dc0caacdcb41b6e509db25dc50460
Aakash <aakashmaneak@gmail.com>
2024-02-23 01:29:54 +0530

    added browser logo

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Food ot go on port 3000 Hello World!");
+});
+
+// handling 404 routes
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Available");
 });
 
 app.listen(port, () => {
